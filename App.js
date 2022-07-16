f11d15cb60c518743def0f04dac254ef6f161cd8
Pamesch21 <93684844+Pamesch21@users.noreply.github.com>
2022-07-16 19:16:52 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+
+
+import NavBar from './components/NavBar/NavBar';
+
+
+function App() {
+const styleApp = {padding:'10px 20px', }
+  return (
+    //JSX
+    <div className="container" style= {styleApp}>
+          <NavBar />
+        
+    </div>
+  );
+}
+
+export default App;
